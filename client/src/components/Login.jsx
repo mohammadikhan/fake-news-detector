b900e3ed6ea902984bc8885aef578224b44d6f4b
Mohammad Khan <mohammadikhan821@gmail.com>
2025-06-29 22:59:20 -0400

    Added Auth Layout component to Login Component so only right side is updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,28 +1,14 @@
 import { ShieldAlert } from "lucide-react";
 import { useState } from "react";
 import DarkModeToggle from "react-dark-mode-toggle"
+import AuthLayout from "./ui/AuthLayout";
 
 const Login = () => {
-  
-  return (
     
-    
-        <div className="min-h-screen flex">
-            {/* Left Side */}
-            <div className="hidden md:flex md:w-1/2 bg-gray-600 items-center justify-center p-12">
-                <div className="max-w-md text-white">
-                    <div className="flex items-center gap-2 mb-8">
-                        <ShieldAlert className="w-40 h-40 text-red-400"/>
-                    <div>
-                        <h1 className="text-4xl font-bold">FakeNews AI</h1>
-                        <p className="text-base text-gray-300 mt-1 dark:text-gray-400">Powered by AI Detection</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-                    
-            
-            {/* Right Side */}
+    return (
+        <>
+            <AuthLayout>
+                {/* Right Side */}
             <div className="w-full md:w-1/2 flex items-center bg-blue-200 justify-center p-8">
                 <div className="w-full max-w-md">
                     <div className="md:hidden flex justify-center mb-10">
@@ -73,7 +59,7 @@ const Login = () => {
                         <div className="mt-6 text-center">
                             <p className="text-sm text-gray-600">
                                 Don't have an account?{' '}
-                                <a href="#" className="text-blue-500 hover:underline">
+                                <a href="/register" className="text-blue-500 hover:underline">
                                     Register
                                 </a>
                             </p>
@@ -83,7 +69,14 @@ const Login = () => {
 
                 </div>
             </div>
-        </div>
+
+            </AuthLayout>
+        </>
+        
+            
+            
+            
+    
         
         
     
