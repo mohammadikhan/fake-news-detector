0e796b2fdc0ecd5329c1c24bdf0ad3c91bd264b5
Mohammad Khan <mohammadikhan821@gmail.com>
2025-08-06 23:41:21 -0400

    Added main components to App to see their appearance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Card from "./components/ui/Card"
+import AnalysisForm from './components/AnalysisForm'
+import Login from './components/Login'
+import AuthLayout from './components/ui/AuthLayout'
+import Register from './components/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Login/>
+      <Register/>
       <Header/>
+      <AnalysisForm/>
     </>
   )
 }
