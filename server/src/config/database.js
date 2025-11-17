0a006a3b57c4d3d07a95bbeb7a6be98a937e83e3
Mohammad Khan <mohammadikhan821@gmail.com>
2025-11-17 15:54:47 -0500

    Added Mongo DB connection to DB config

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,1 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`[SUCCESS-CONNECTED]: MongoDB Connected --> ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`[ERROR]: ${error.message}`);
+        process.exit(1);
+    }
+}
