93c21aea852235863b1eff071ca677d9317c201e
Mohammad Khan <mohammadikhan821@gmail.com>
2025-11-22 20:43:16 -0500

    Added verify authentication route

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../controllers/authController.js"
+import { registerUser, verifyUser } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post("/register", registerUser);
+router.post("/verify", verifyUser);
 
 export default router;
