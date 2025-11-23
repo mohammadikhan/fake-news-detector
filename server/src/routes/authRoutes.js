f257a5e52e70cc0c17f1aa6d08f5f4a27ae5195b
Mohammad Khan <mohammadikhan821@gmail.com>
2025-11-23 00:35:40 -0500

    Added resend_code authentication route

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { registerUser, verifyUser } from "../controllers/authController.js"
+import { registerUser, resendVerificationCode, verifyUser } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post("/register", registerUser);
 router.post("/verify", verifyUser);
+router.post("/resend_code", resendVerificationCode);
 
 export default router;
