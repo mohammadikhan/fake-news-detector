7bdcf09897c467d6bf8d5bb747b9dd2d8bb4da5d
Mohammad Khan <mohammadikhan821@gmail.com>
2025-12-14 23:58:20 -0500

    Added attach access token middleware

--- server/src/middleware/attachAccessToken.js
+++ server/src/middleware/attachAccessToken.js
@@ -4,6 +4,30 @@ import generateTokens from '../utils/generateJWT.js';
 
 const attachAccessToken = async(req, res, next) => {
 
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(401).json({message: "[ERROR]: Session Token not found. Authentication is required"});
+    }
+
+    try {
+
+        const tokenInDB = await RefreshToken.findOne({ token: refreshToken });
+
+        if (!tokenInDB) {
+            return res.status(401).json({ message: "[ERROR]: Token is invalid or the session token has been revoked."});
+        }
+
+        const { accessToken } = generateTokens(tokenDocument.userId);
+
+        req.accessToken = accessToken;
+
+        next();
+
+    } catch (error) {
+        console.error("[ERROR]: Error in generating/attaching Access Token: ", error);
+        return res.status(500).json({ message: "[SERVER-ERROR]: Server error during session refresh"});
+    }
 }
 
 export default attachAccessToken;
