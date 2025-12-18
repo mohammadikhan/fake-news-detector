929adb0e07b2d4c88a0db0555681b3cb88444eeb
Mohammad Khan <mohammadikhan821@gmail.com>
2025-12-17 20:32:07 -0500

    Fixed variable name when generating access token and cleaned code

--- server/src/middleware/attachAccessToken.js
+++ server/src/middleware/attachAccessToken.js
@@ -4,27 +4,28 @@ import generateTokens from '../utils/generateJWT.js';
 
 const attachAccessToken = async(req, res, next) => {
 
-    const refreshToken = req.cookies.refreshToken;
-
-    if (!refreshToken) {
-        return res.status(401).json({message: "[ERROR]: Session Token not found. Authentication is required"});
-    }
-
     try {
+
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            return res.status(401).json({success: false, message: "[ERROR]: Session Token not found. Authentication is required"});
+        }
 
         const tokenInDB = await RefreshToken.findOne({ token: refreshToken });
 
         if (!tokenInDB) {
-            return res.status(401).json({ message: "[ERROR]: Token is invalid or the session token has been revoked."});
+            return res.status(401).json({sucess: false, message: "[ERROR]: Token is invalid or the session token has been revoked."});
         }
 
-        const { accessToken } = generateTokens(tokenDocument.userId);
+        const { accessToken } = generateTokens(tokenInDB.userId.toString());
 
         req.accessToken = accessToken;
+        req.userId = tokenInDB.userId.toString();
 
         next();
 
-    } catch (error) {
+    } catch(error) {
         console.error("[ERROR]: Error in generating/attaching Access Token: ", error);
         return res.status(500).json({ message: "[SERVER-ERROR]: Server error during session refresh"});
     }
