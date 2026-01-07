808f57bdea879eb17c4e5cdca37d879de321c192
Mohammad Khan <mohammadikhan821@gmail.com>
2026-01-06 23:57:25 -0500

    Added initial fields for analysis schema

--- server/src/models/Analysis.js
+++ server/src/models/Analysis.js
@@ -2,7 +2,49 @@ import mongoose from "mongoose";
 
 const AnalysisSchema = new mongoose.Schema({
 
-    
-})
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        default: null
+    },
+
+    inputText: {
+        type: String,
+        required: true,
+        minLength: 50
+    },
+
+    prediction: {
+        type: String,
+        enum: ["FAKE, REAL"],
+        required: true
+    },
+
+    confidence: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 1
+    },
+
+    probabilities: {
+        
+        fake: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 1
+        },
+
+        real: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 1
+        }
+
+    },
+
+}, { timestamps: true });
 
 export default mongoose.model("Analysis", AnalysisSchema)
