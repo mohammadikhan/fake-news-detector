4d6be0b637dab26c77970869a091a3e08915f882
Mohammad Khan <mohammadikhan821@gmail.com>
2026-01-28 13:44:32 -0500

    Added feedback controller to allow users to submit feedback and attach it to its corresponding analysis

---
+++ server/src/controllers/feedbackController.js
@@ -0,0 +1,40 @@
+import Analysis from "../models/Analysis.js";
+
+export const submitFeedback = async(req, res) => {
+
+    try {
+
+        const { id } = req.params;
+        const { isCorrect, userPrediction, comment } = req.body;
+        const userId = req.userId;
+
+        const analysis = await Analysis.findById(id);
+
+        if (!analysis) {
+            return res.status(404).json({message: "[ERROR]: Analysis was not found"});
+        }
+
+        if (analysis.user?.toString() !== userId) {
+            return res.status(403).json({message: "[ERROR]: User did not make this analysis"});
+        }
+
+        if (analysis.feedback) {
+            return res.status(400).json({message: "[ERROR]: Feedback has already been submitted for this analysis. Multiple feedbacks cannot be done to the same analysis"});
+        }
+
+        analysis.feedback = {
+            isCorrect,
+            userPrediction,
+            comment
+        }
+
+        console.log(userId);
+        await analysis.save();
+
+        res.status(200).json({success: true, feedback: analysis.feedback});
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "[ERROR]: Server Error"});
+    }
+};
