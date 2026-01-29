99132ebc91b9cdb7bfde8eb278154e13e6cc7222
Mohammad Khan <mohammadikhan821@gmail.com>
2026-01-29 12:47:52 -0500

    Added analysis routes to analyze news articles, submit feedback and get analyses that have feeedback submitted

--- server/src/routes/analysisRoutes.js
+++ server/src/routes/analysisRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { analyze } from "../controllers/analysisController.js";
+import attachAccessToken from "../middleware/attachAccessToken.js";
+import verifyToken from "../middleware/verifyToken.js";
+import checkBlacklist from "../middleware/checkBlacklist.js";
+import attachUser from "../middleware/attachUser.js";
+import { getAnalysisFeedback } from "../controllers/getAnalysisFeedback.js";
+import { submitFeedback } from "../controllers/feedbackController.js";
 
 const router = express.Router();
 
-router.post("/text", analyze);
+router.post("/text", attachAccessToken, verifyToken, checkBlacklist, attachUser, analyze);
+router.post("/:id/feedback", attachAccessToken, verifyToken, checkBlacklist, submitFeedback);
+router.get("/:id/feedback", attachAccessToken, verifyToken, checkBlacklist, getAnalysisFeedback);
 
 export default router;
