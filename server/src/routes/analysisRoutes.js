e31cff768e23e08d95b50cb1e3f16db085c9743f
Mohammad Khan <mohammadikhan821@gmail.com>
2026-01-30 14:12:52 -0500

    Removed unused imports and routers (gave feedback its own routes

--- server/src/routes/analysisRoutes.js
+++ server/src/routes/analysisRoutes.js
@@ -4,13 +4,10 @@ import attachAccessToken from "../middleware/attachAccessToken.js";
 import verifyToken from "../middleware/verifyToken.js";
 import checkBlacklist from "../middleware/checkBlacklist.js";
 import attachUser from "../middleware/attachUser.js";
-import { getAnalysisFeedback } from "../controllers/getAnalysisFeedback.js";
-import { submitFeedback } from "../controllers/feedbackController.js";
 
 const router = express.Router();
 
 router.post("/text", attachAccessToken, verifyToken, checkBlacklist, attachUser, analyze);
-router.post("/:id/feedback", attachAccessToken, verifyToken, checkBlacklist, submitFeedback);
-router.get("/:id/feedback", attachAccessToken, verifyToken, checkBlacklist, getAnalysisFeedback);
+
 
 export default router;
