bdaaf85bdf233014adcbbf6860509353df0387b5
Mohammad Khan <mohammadikhan821@gmail.com>
2026-03-09 23:15:28 -0400

    Added route to get number of users so that it can be displayed on landing page

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
+import { getNumUsers, getUserProfile } from "../controllers/userController.js";
 import verifyToken from "../middleware/verifyToken.js";
 import checkBlacklist from "../middleware/checkBlacklist.js";
 import attachAccessToken from "../middleware/attachAccessToken.js";
@@ -7,5 +7,6 @@ import attachAccessToken from "../middleware/attachAccessToken.js";
 const router = express.Router();
 
 router.get("/profile", attachAccessToken, verifyToken, checkBlacklist, getUserProfile);
+router.get("/stats", getNumUsers);
 
 export default router;
