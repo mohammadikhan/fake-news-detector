f0914f92249c4e9a7180d85ad3aa842405e4b9f6
Mohammad Khan <mohammadikhan821@gmail.com>
2026-04-14 00:29:09 -0400

    Created stats route to get total number of analyses performed by the application to be displayed on the Landing Page

--- server/src/routes/analysisRoutes.js
+++ server/src/routes/analysisRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { analyze } from "../controllers/analysisController.js";
+import { analyze, statistics } from "../controllers/analysisController.js";
 import attachAccessToken from "../middleware/attachAccessToken.js";
 import verifyToken from "../middleware/verifyToken.js";
 import checkBlacklist from "../middleware/checkBlacklist.js";
@@ -8,6 +8,7 @@ import attachUser from "../middleware/attachUser.js";
 const router = express.Router();
 
 router.post("/text", attachAccessToken, verifyToken, checkBlacklist, attachUser, analyze);
+router.get("/stats", statistics);
 
 
 export default router;
