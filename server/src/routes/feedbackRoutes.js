099cd0643a1ffbb438183434632c00182f725c06
Mohammad Khan <mohammadikhan821@gmail.com>
2026-04-16 13:57:44 -0400

    Added attachUser import and middleware to all 3 feedback routes

--- server/src/routes/feedbackRoutes.js
+++ server/src/routes/feedbackRoutes.js
@@ -2,14 +2,15 @@ import express from "express";
 import attachAccessToken from "../middleware/attachAccessToken.js";
 import verifyToken from "../middleware/verifyToken.js";
 import checkBlacklist from "../middleware/checkBlacklist.js";
+import attachUser from "../middleware/attachUser.js";
 import { getAnalysisFeedback } from "../controllers/getAnalysisFeedback.js";
 import { submitFeedback } from "../controllers/feedbackController.js";
 import { getUserFeedback } from "../controllers/getUserFeedback.js";
 
 const router = express.Router();
 
-router.post("/", attachAccessToken, verifyToken, checkBlacklist, submitFeedback);
-router.get("/my-reports", attachAccessToken, verifyToken, checkBlacklist, getUserFeedback);
-router.get("/:id", attachAccessToken, verifyToken, checkBlacklist, getAnalysisFeedback);
+router.post("/", attachAccessToken, verifyToken, checkBlacklist, attachUser, submitFeedback);
+router.get("/my-reports", attachAccessToken, verifyToken, checkBlacklist, attachUser, getUserFeedback);
+router.get("/:id", attachAccessToken, verifyToken, checkBlacklist, attachUser, getAnalysisFeedback);
 
 export default router;
