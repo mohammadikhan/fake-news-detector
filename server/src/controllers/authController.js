d7f966200b1d99f5d50c5742914120d4533d7d1c
Mohammad Khan <mohammadikhan821@gmail.com>
2026-04-17 22:58:17 -0400

    Fix cookie sameSite for cross-origin production requests

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -135,7 +135,7 @@ export const login = async(req, res) => {
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         maxAge: maxAge,
     });
 
@@ -174,7 +174,7 @@ export const logout = async(req, res) => {
         res.clearCookie('refreshToken', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
 
         res.status(200).json({
